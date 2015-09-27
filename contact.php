4b0bc85e1e1b582202305c97eee2a812ab3ed385
bradlavario <blavario@gmail.com>
2015-09-27 16:07:25 -0400

    Create contact.php
    Main contact form, taken from c0dePen, so you will need to link the css included or create your own.

---
+++ contact.php
@@ -0,0 +1,35 @@
+<div id="section-container">
+  
+  <div id="section-title">
+    <h1>Contact</h1>
+    </div>
+  
+<div id="page-wrap">
+
+		<h3>Drop me a line...</h3>
+	
+<div id="contact-area">
+			
+			<form method="post" action="contactengine.php">
+				<label for="Name">Name:</label>
+				<input type="text" name="Name" id="Name" />
+				
+				<label for="City">Phone:</label>
+				<input type="text" name="Tel" id="Tel" />
+	
+				<label for="Email">Email:</label>
+				<input type="text" name="Email" id="Email" />
+				
+				<label for="Message">Message:</label><br />
+				<textarea name="Message" rows="20" cols="20" id="Message"></textarea>
+
+				<input type="submit" name="submit" value="Submit" class="submit-button" />
+			</form>
+			
+  <div style="clear: both;"></div>
+			
+			<h4>-or- email me at <a href="mailto:your.email@somewhere.com">your.email@somewhere.com</a></h4>
+		
+		</div>
+  </div>
+	</div>
